3e8734e6a8f27b35ff4253170ba168c59885e995
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:17:38 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,17 @@
-print('hello')
-print('world')
-print('world')
+# The init Method 
+# inside class Time:
+  
+  def _init_(self, hour = 0, minutes = 0, second = 0)
+      self.hour = hour 
+      self.minute = minute
+      self.second = second
+      
+      self.hour = hour
+ 
+time = Time()
+time.print_time()
+time = Time(9)
+time.print_time()
+time = Time(9, 45)
+time.print_time()
+09:45:00
